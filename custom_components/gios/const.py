95bda2d1e284e3f5ad95f224bac94bf3cec4d059
bieniu <maciej.bieniek@gmail.com>
2019-09-10 15:45:32 +0200

    Sort consts

--- custom_components/gios/const.py
+++ custom_components/gios/const.py
@@ -1,10 +1,6 @@
 """Constants for GIOS integration."""
-
-DOMAIN = "gios"
-DEFAULT_NAME = "GIOŚ"
-
-CONF_STATION_ID = "station_id"
-
-STATIONS_URL = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
-
 ATTR_ID = "id"
+CONF_STATION_ID = "station_id"
+DEFAULT_NAME = "GIOŚ"
+DOMAIN = "gios"
+STATIONS_URL = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"

044088dec8e1c461c30f3cfeb9211e3545013737
Maciej Bieniek <maciej.bieniek@gmail.com>
2019-10-26 11:55:07 +0200

    First complete version

--- custom_components/gios/pygios/__init__.py
+++ custom_components/gios/pygios/__init__.py
@@ -1,39 +1,31 @@
 """
 Python wrapper for getting air quality data from GIOS.
 """
-import aiohttp
-import asyncio
 import logging
 
-_LOGGER = logging.getLogger(__name__)
+from aiohttp import ClientError
 
-INDEXES_URL = "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/{}"
-SENSOR_URL = "http://api.gios.gov.pl/pjp-api/rest/data/getData/{}"
-STATION_URL = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/{}"
-STATIONS_URL = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
-HTTP_OK = "200"
+_LOGGER = logging.getLogger(__name__)
 
 ATTR_AQI = "AQI"
 ATTR_ID = "id"
 ATTR_INDEX = "index"
-ATTR_NAME = "name"
-ATTR_STATION = "station"
-ATTR_VALUE = "value"
-ATTR_GEGR_LAT = "gegrLat"
-ATTR_GEGR_LON = "gegrLon"
 ATTR_INDEX_LEVEL = "{}IndexLevel"
-ATTR_INDEX_LEVEL_NAME = "indexLevelName"
-ATTR_PARAM = "param"
-ATTR_PARAM_CODE = "paramCode"
-ATTR_PARAM_NAME = "paramName"
-ATTR_ST_INDEX_LEVEL = "stIndexLevel"
-ATTR_STATION_NAME = "stationName"
-ATTR_VALUES = "values"
+ATTR_NAME = "name"
+ATTR_VALUE = "value"
 
-class Gios():
+HTTP_OK = 200
+URL_INDEXES = "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/{}"
+URL_SENSOR = "http://api.gios.gov.pl/pjp-api/rest/data/getData/{}"
+URL_STATION = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/{}"
+URL_STATIONS = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
+
+
+class Gios:
     """Main class to perform GIOS API requests"""
 
-    def __init__(self, station_id, session: aiohttp.ClientSession):
+    def __init__(self, station_id, session):
+        """Initialize."""
         self._data = {}
         self._available = False
         self._station_id = station_id
@@ -44,55 +36,79 @@ class Gios():
         self.session = session
 
     async def update(self):
-        stations = await self._async_get(STATIONS_URL)
-        if not stations:
-            return
+        """Update GIOS data."""
+        if not self._station_name:
+            stations = await self._async_get(URL_STATIONS)
+            if not stations:
+                return
 
-        for station in stations:
-            if station[ATTR_ID] == self._station_id:
-                self._latitude = station[ATTR_GEGR_LAT]
-                self._longitude = station[ATTR_GEGR_LON]
-                self._station_name = station[ATTR_STATION_NAME]
+            for station in stations:
+                if station[ATTR_ID] == self._station_id:
+                    self._latitude = station["gegrLat"]
+                    self._longitude = station["gegrLon"]
+                    self._station_name = station["stationName"]
 
-        url = STATION_URL.format(self._station_id)
-        station_data = await self._async_get(url)
-        if not station_data:
-            return
-        for sensor in station_data:
-            self._data[sensor[ATTR_PARAM][ATTR_PARAM_CODE]] = {
-                ATTR_ID: sensor[ATTR_ID],
-                ATTR_NAME: sensor[ATTR_PARAM][ATTR_PARAM_NAME],
-            }
+            url = URL_STATION.format(self._station_id)
+            station_data = await self._async_get(url)
+            if not station_data:
+                return
+            for sensor in station_data:
+                self._data[sensor["param"]["paramCode"]] = {
+                    ATTR_ID: sensor[ATTR_ID],
+                    ATTR_NAME: sensor["param"]["paramName"],
+                }
 
         for sensor in self._data:
             if sensor != ATTR_AQI:
-                url = SENSOR_URL.format(self._data[sensor][ATTR_ID])
+                url = URL_SENSOR.format(self._data[sensor][ATTR_ID])
                 sensor_data = await self._async_get(url)
-                sensor_data = sensor_data[ATTR_VALUES]
+                sensor_data = sensor_data["values"]
                 self._data[sensor][ATTR_VALUE] = sensor_data[0][ATTR_VALUE]
 
+        url = URL_INDEXES.format(self._station_id)
+        indexes = await self._async_get(url)
+        try:
+            for sensor in self._data:
+                if sensor != ATTR_AQI:
+                    index_level = ATTR_INDEX_LEVEL.format(
+                        sensor.lower().replace(".", "")
+                    )
+                    self._data[sensor][ATTR_INDEX] = indexes[index_level][
+                        "indexLevelName"
+                    ].lower()
+
+            self._data[ATTR_AQI] = {ATTR_NAME: ATTR_AQI}
+            self._data[ATTR_AQI][ATTR_VALUE] = indexes["stIndexLevel"][
+                "indexLevelName"
+            ].lower()
+        except (TypeError, IndexError, TypeError):
+            _LOGGER.error("Invalid data from GIOS API")
 
     async def _async_get(self, url):
-        """Retreive data from GIOS site via aiohttp."""
+        """Retreive data from GIOS API."""
         data = None
-        # try:
-        async with self.session.get(url) as response:
-            data = await response.json()
-        # except aiohttp.ClientError as error:
-        #     _LOGGER.error("Could not fetch data from %s, error: %s", url, error)
-        #     return
-        # if resp.status != HTTP_OK:
-        #     _LOGGER.error("Could not fetch data from %s, status: %s", url, resp.status)
-        # else:
-        #     _LOGGER.debug("Data retrieved from %s, status: %s", url, resp.status)
+        try:
+            async with self.session.get(url) as response:
+                if response.status != HTTP_OK:
+                    _LOGGER.warning(
+                        "Invalid response from GIOS API: %s", response.status
+                    )
+                    raise GiosError(response.status, await response.text())
+                data = await response.json()
+        except ClientError as error:
+            _LOGGER.error("Invalid response from from GIOS API: %s", error)
+            return
+        _LOGGER.debug("Data retrieved from %s, status: %s", url, response.status)
         return data
 
     @property
     def data(self):
+        """Return the data."""
         return self._data
 
     @property
     def available(self):
+        """Return True is data is available."""
         if len(self._data) > 0:
             self._available = True
         else:
@@ -101,19 +117,25 @@ class Gios():
 
     @property
     def latitude(self):
+        """Return latitude of measuring station."""
         return self._latitude
 
     @property
     def longitude(self):
+        """Return longitude of measuring station."""
         return self._longitude
 
     @property
     def station_name(self):
+        """Return name of measuring station."""
         return self._station_name
 
-# class GiosError(Exception):
-#     """Raised when GIOS API request ended in error."""
 
-#     def __init__(self, status_code, status):
-#         self.status_code = status_code
-#         self.status = status
+class GiosError(Exception):
+    """Raised when GIOS API request ended in error."""
+
+    def __init__(self, status_code, status):
+        """Initialize."""
+        Exception.__init__(self, status_code, status)
+        self.status_code = status_code
+        self.status = status
--- custom_components/gios/start.py
+++ custom_components/gios/start.py
@@ -1,22 +1,32 @@
-import aiohttp
+from aiohttp import ClientSession
 import asyncio
 
 from pygios import Gios
+from pygios import GiosError
 
 
 async def main():
-    async with aiohttp.ClientSession() as websession:
-        gios = Gios(11794, websession)
-        await gios.update()
-        data = gios.data
-        available = gios.available
-        latitude = gios.latitude
-        longitude = gios.longitude
-        station_name = gios.station_name
+    try:
+        async with ClientSession() as websession:
+            gios = Gios(11794, websession)
+            await gios.update()
+            data = gios.data
+            available = gios.available
+            latitude = gios.latitude
+            longitude = gios.longitude
+            station_name = gios.station_name
 
-    print(f"Data available: {available}")
-    print(f"Longitude: {longitude}, latitude: {latitude}, station name: {station_name}")
-    print(data)
+            print(f"Data available: {available}")
+            print(f"Longitude: {longitude}, latitude: {latitude}, station name: {station_name}")
+            print(data)
+
+            await gios.update()
+
+            print(f"Data available: {available}")
+            print(f"Longitude: {longitude}, latitude: {latitude}, station name: {station_name}")
+            print(data)
+    except GiosError:
+        print(f"{GiosError.status_code}, {GiosError.status}")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())

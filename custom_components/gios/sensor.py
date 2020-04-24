891b9fa139363651b475a71e887f1904994b4c12
bieniu <maciej.bieniek@gmail.com>
2020-04-24 11:11:31 +0200

    Black

--- custom_components/gios/sensor.py
+++ custom_components/gios/sensor.py
@@ -128,7 +128,9 @@ class GiosSensor(Entity):
 
     async def async_added_to_hass(self):
         """Connect to dispatcher listening for entity data notifications."""
-        self.async_on_remove(self.coordinator.async_add_listener(self.async_write_ha_state))
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
 
     async def async_update(self):
         """Update GIOS entity."""
